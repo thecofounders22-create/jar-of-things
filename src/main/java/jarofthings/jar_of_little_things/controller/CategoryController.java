c4771707c03b37adf62fc3912421ef398b4e340f
aaradhy22 <aaradhyavairagi@gmail.com@email.com>
2026-09-20 13:23:24 +0530

    Allow deployed frontend in CORS

--- src/main/java/jarofthings/jar_of_little_things/controller/CategoryController.java
+++ src/main/java/jarofthings/jar_of_little_things/controller/CategoryController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/categories")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "https://jar-of-things-frontend.onrender.com")
 public class CategoryController {
 
     private final CategoryService categoryService;
